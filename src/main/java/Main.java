8f4b5490370016726dbaa18ec5b71d93c7838d47
[Bartosz Bala] <[bala.bartosz@gmail.com]>
2019-09-13 21:15:18 +0200

    add prime number 2 to final array

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,19 @@
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Main {
 
-    static List<Integer> numberWithoutEven(int i) {
-        List<Integer> lista = new ArrayList<Integer>();
-        return lista = IntStream.range(3, i).filter(x -> x % 2 != 0).boxed().collect(Collectors.toList());
+    private static List<Integer> numberWithoutEven(int i) {
+        return IntStream.range(3, i).filter(x -> x % 2 != 0).boxed().collect(Collectors.toList());
     }
 
 
-    static int[] returnPrimeNumber(List<Integer> number) {
+    private static int[] returnPrimeNumber(List<Integer> number) {
         int[] eventab = number.stream().mapToInt(i -> i).toArray();
-        Double d = (double) eventab[eventab.length - 1];
+        double d = eventab[eventab.length - 1];
         int i = 0;
         while (i < Math.sqrt(d)) {
             for (int k = 0; k <= eventab.length - 2; ) {
@@ -34,15 +32,15 @@ public class Main {
         return eventab;
     }
 
-    static int[] returnPrimeNumberWithou0(int[] tabPrimeWith0) {
-        int[] primeNumber = new int[0];
+    private static int[] returnPrimeNumberWithou0(int[] tabPrimeWith0) {
+        int[] primeNumber = {2};
 
 
-        int j = 0;
-        for (int i = 0; i < tabPrimeWith0.length; i++) {
-            if (tabPrimeWith0[i] != 0) {
+        int j = 1;
+        for (int value : tabPrimeWith0) {
+            if (value != 0) {
                 primeNumber = Arrays.copyOf(primeNumber, primeNumber.length + 1);
-                primeNumber[j] = tabPrimeWith0[i];
+                primeNumber[j] = value;
 
                 j++;
             }
@@ -51,8 +49,9 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        Main mn = new Main();
-
-        System.out.println(Arrays.toString(returnPrimeNumberWithou0(returnPrimeNumber(numberWithoutEven(500000)))));
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Put max value of prime number which would you like to print in array");
+        int n =sc.nextInt();
+        System.out.println(Arrays.toString(returnPrimeNumberWithou0(returnPrimeNumber(numberWithoutEven(n)))));
     }
 }
